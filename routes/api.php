34b125afb825689f06ce053f02f8f11fbf15bbff
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2026-04-21 16:28:51 -0300

    feat: added a route to test relationship Worker/Enterprise

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\WorkerResponse;
+use App\Models\Enterprise;
 
-Route::get("/test", function() { return "banana"; });
+Route::get("/getWorkersFrom/{id}", function(int $id) {
+	return WorkerResponse::collection(Enterprise::find($id)->workers);
+});
