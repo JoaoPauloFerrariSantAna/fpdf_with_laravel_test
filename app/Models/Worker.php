b4f0be4a1f774e0849909318c7e5372e5946307c
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2026-04-21 16:26:31 -0300

    feat: added a new trait to Worker

--- app/Models/Worker.php
+++ app/Models/Worker.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Workers extends Model
+class Worker extends Model
 {
-    use SoftDeletes;
+	use SoftDeletes;
+	use HasFactory;
 
 	protected $table = "workers_tbl";
-    protected $fillable = array("name", "email", "sector", "enterprise_id");
+	protected $fillable = array("name", "email", "sector", "enterprise_id");
 }
